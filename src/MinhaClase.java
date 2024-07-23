c147ed838d19566a27354db92db7fa0c9bc7ff7f
Lucas Suzuki <lucassuzuki13@gmail.com>
2024-07-22 21:03:24 -0300

    feat: add arquivo BoletimEstudantil.java

---
+++ src/MinhaClase.java
@@ -0,0 +1,14 @@
+public class MinhaClase {
+    public static void main(String[] args) {
+        String primeiroNome = "Lucas";
+        String segundoNome = "Suzuki";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.print(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
