6333ccce45319f1056e603ae75235d6876069fa7
Lucas Suzuki <lucassuzuki13@gmail.com>
2024-07-23 21:51:18 -0300

    feat: add arquivo AboutMe.java

---
+++ src/AboutMe.java
@@ -0,0 +1,26 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+         
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu Sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite a sua altura: ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é de " + altura + "cm");
+    }
+}
