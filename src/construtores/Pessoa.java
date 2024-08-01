ebbbae345015e69e1988867e834c293337bd6a10
Lucas Suzuki <lucassuzuki13@gmail.com>
2024-08-01 18:54:39 -0300

    feat: add package construtores

---
+++ src/construtores/Pessoa.java
@@ -0,0 +1,29 @@
+package construtores;
+
+public class Pessoa {
+	private String nome;
+	private String cpf;
+	private String endereco;
+	
+	public Pessoa(String cpf, String nome) {
+		this.cpf = cpf;
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public String getEndereco() {
+		return endereco;
+	}
+	
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+}
